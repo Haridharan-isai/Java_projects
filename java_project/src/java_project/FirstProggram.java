b3a50cc2532c21c214ca6ad1cbfba165fd2d6dbf
Haridharan-isai <harishthagam@gmail.com>
2024-12-12 22:10:51 +0530

    second one

---
+++ java_project/src/java_project/FirstProggram.java
@@ -0,0 +1,16 @@
+package java_project;
+
+public class FirstProggram {
+
+	public static void main(String[] args) {
+	int num=10;
+	if(num%10==0) {
+		System.out.println("even number");
+	}
+	else {
+		System.out.println("odd number");
+	}
+
+	}
+
+}
