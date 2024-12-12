1ec8e6cabed617bbf20133876df078ce763e6258
Haridharan-isai <harishthagam@gmail.com>
2024-12-12 22:18:43 +0530

    hello world program

--- java_project/src/java_project/helloWorld.java
+++ java_project/src/java_project/helloWorld.java
@@ -6,5 +6,6 @@ public class helloWorld {
 		System.out.println("helloworld");
 		System.out.println("new to the GIT & GITHUB");
 		System.out.println("one more to the git");
+		System.out.println("thank in the hello world");
 	}
 }
